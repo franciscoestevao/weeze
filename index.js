13a4cbb4a15e5a7ae44b080c7807f524e8b06f0f
franciscoestevao <franciscoestevao@gmail.com>
2015-11-26 01:01:20 +0000

    tentativa de index

---
+++ index.js
@@ -0,0 +1,15 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+jQuery(document).ready(function($) {
+  tab = $('.tabs h3 a');
+
+  tab.on('click', function(event) {
+    event.preventDefault();
+    tab.removeClass('active');
+    $(this).addClass('active');
+
+    tab_content = $(this).attr('href');
+    $('div[id$="tab-content"]').removeClass('active');
+    $(tab_content).addClass('active');
+  });
+});
