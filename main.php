a78ad3f163e23e997fc696b875a5afed9daa9fb2
franciscoestevao <franciscoestevao@gmail.com>
2015-11-27 01:27:37 +0000

    tirei a cena de londres

--- main.php
+++ main.php
@@ -44,15 +44,11 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
         </div>
 
         <div id="section">
-        <h1>London</h1>
+        <h1>Latest Events</h1>
         <p>
-        London is the capital city of England. It is the most populous city in the United Kingdom,
-        with a metropolitan area of over 13 million inhabitants.
+			Here are the latest events:
         </p>
-        <p>
-        Standing on the River Thames, London has been a major settlement for two millennia,
-        its history going back to its founding by the Romans, who named it Londinium.
-        </p>
+       
         </div>
 
         <div id="footer">
