53f8078e6275ec73e1d76ee52b13c9a303b744d8
tavarest <tomasfbtavares@gmail.com>
2015-12-02 15:35:42 +0000

    pagination a funcionar excepto noo search and filter

--- main/main.php
+++ main/main.php
@@ -69,11 +69,42 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
         </div>
         
         <?php 
-			global $db;
-			
-			$stmt = $db->prepare("SELECT * FROM evento order by id DESC");
-			$stmt->execute();
-			$result = $stmt->fetchAll();
+			global $db;        
+            //PAGINATION
+            // Find out how many items are in the table
+            $total = $db->query('SELECT COUNT(*) FROM evento')->fetchColumn();
+            $limit =9;
+            $pages = ceil($total/$limit);
+
+
+            // What page are we currently on?
+            $page = min($pages, filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT, array('options' => array('default'   => 1,'min_range' => 1,),)));
+
+            // Calculate the offset for the query
+            $offset = ($page - 1)  * $limit;
+
+            // Some information to display to the user
+            $start = $offset + 1;
+            $end = min(($offset + $limit), $total);
+
+            // The "back" link
+            $prevlink = ($page > 1) ? '<a href="?page=1" title="First page">&laquo;</a> <a href="?page=' . ($page - 1) . '" title="Previous page">&lsaquo;</a>' : '<span class="disabled">&laquo;</span> <span class="disabled">&lsaquo;</span>';
+
+            // The "forward" link
+            $nextlink = ($page < $pages) ? '<a href="?page=' . ($page + 1) . '" title="Next page">&rsaquo;</a> <a href="?page=' . $pages . '" title="Last page">&raquo;</a>' : '<span class="disabled">&rsaquo;</span> <span class="disabled">&raquo;</span>';
+
+
+            // Display the paging information
+        //echo '<div id="paging"><p>', $prevlink, ' Page ', $page, ' of ', $pages, ' pages, displaying ', $start, '-', $end, ' of ', $total, ' results ', $nextlink, ' </p></div>';
+
+            // Prepare the paged query
+            $stmt = $db->prepare('SELECT * FROM evento ORDER BY id DESC LIMIT :limit OFFSET :offset');
+            // Bind the query params
+            $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
+            $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
+            $stmt->execute();
+
+            $result = $stmt->fetchAll();
         ?>
 
         <div id="section">
@@ -81,8 +112,9 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
         <h5>
 			Here are the latest events:
         </h5>
-        
-        <ul>
+        <div class="allevents">
+            
+           <ul>
 			<?php
 				if(count($result)){
 				foreach ($result as $row){
@@ -107,8 +139,13 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
             </div>
            
 			<?php }} ?>
-		</ul>
-       
+		</ul> 
+            
+        </div>
+        
+            <div class="pagination">
+            <p><?php echo '<div id="paging"><p>', $prevlink, ' Page ', $page, ' of ', $pages, $nextlink, ' </p></div>';?></p>    
+            </div>
         </div>
 
         <div id="footer">
