ad44fa627d8d1bb2d575f1c536271509f2db53dd
tavarest <tomasfbtavares@gmail.com>
2015-12-02 12:48:04 +0000

    com md5 e com alteração da password

--- action/new_config.php
+++ action/new_config.php
@@ -1,17 +1,31 @@
 <?php
 include_once('../database/connection.php'); // connects to the database
-session_start();                         // starts the session        
+session_start();                         // starts the session    
 
-$password=trim($_POST['pass']);
-$cpassword=trim($_POST['pass']);
 
-if($password !== $cpassword) {
-            //echo "<script> window.location.assign('../errors/password_error.php'); </script>";
-			header('Location: ../errors/password_error.php');
-		}
-else{
+
+if(isset($_POST['submit'])){
+    global $db;
+    $password=trim($_POST['pass']);
+    $cpassword=trim($_POST['cpass']);
+
+    if($password != $cpassword) {
+                //echo "<script> window.location.assign('../errors/password_error.php'); </script>";
+                header('Location: ../errors/password_error.php');
+            }
+    else{
+        $username= $_SESSION['username'];
+        $password = md5($password);
+        $stmt = $db->prepare("UPDATE utilizador SET password=:password WHERE username=:username");
+        $stmt->bindParam(':password', $password);
+        $stmt->bindParam(':username', $username);
+        if($stmt->execute()){
+				header('Location: ../main/main.php');
+			}
+
+    } 
     
-    $password = md5($password);
 }
+
 
 ?>
