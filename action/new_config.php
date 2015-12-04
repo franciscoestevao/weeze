50a7c73ddc1799fbdd19f59177cd78899e8ea876
franciscoestevao <franciscoestevao@gmail.com>
2015-12-04 00:00:44 +0000

    new password ja pede a password antiga

--- action/new_config.php
+++ action/new_config.php
@@ -8,8 +8,14 @@ if(isset($_POST['submit'])){
     global $db;
     $password=trim($_POST['pass']);
     $cpassword=trim($_POST['cpass']);
+    $old = trim($_POST['old']);
+    
+    $stmt = $db->prepare("SELECT * from utilizador WHERE username=:username");
+    $stmt->bindParam(':username', $_SESSION['username']);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
 
-    if($password != $cpassword) {
+    if($password != $cpassword or md5($old) !== $result['password']) {
                 //echo "<script> window.location.assign('../errors/password_error.php'); </script>";
                 header('Location: ../errors/password_error.php');
             }
