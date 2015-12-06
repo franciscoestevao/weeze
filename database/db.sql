f402851bc5f3e376542ea3cfcaaff83f0b1ae797
franciscoestevao <franciscoestevao@gmail.com>
2015-12-06 16:51:11 +0000

    push do script sql

--- database/db.sql
+++ database/db.sql
@@ -1,28 +1,39 @@
 
-
 CREATE TABLE utilizador (
 	username 		VARCHAR PRIMARY KEY NOT NULL,
 	nome 			VARCHAR NOT NULL,
 	password 		VARCHAR NOT NULL,
-	img 			INTEGER NOT NULL
-);
+	img 			INTEGER
+)
 
 CREATE TABLE evento (
-	id 				INTEGER PRIMARY KEY NOT NULL,
-	nome 			VARCHAR NOT NULL,
-	data 			DATE NOT NULL,
-	local 			VARCHAR,
-	descricao 		MEDIUMTEXT NOT NULL,
-	tipo 			VARCHAR NOT NULL,
-	imagem 			LONGBLOB NOT NULL,
-	criador			VARCHAR NOT NULL,
-	FOREIGN KEY (criador) REFERENCES utilizador(username)
-	-- participantes SET
-);
+	id				INTEGER NOT NULL,
+	nome			VARCHAR NOT NULL,
+	data			DATE NOT NULL,
+	local			VARCHAR,
+	descricao		MEDIUMTEXT NOT NULL,
+	tipo			VARCHAR NOT NULL,
+	imagem			LONGBLOB NOT NULL,
+	criador			varchar,
+	privado			BOOLEAN,
+	PRIMARY KEY(id)
+)
+
+
+CREATE TABLE convidado (
+	id				INTEGER NOT NULL,
+	convidado		TEXT NOT NULL
+)
+
+CREATE TABLE participante (
+	id				INTEGER NOT NULL,
+	participante	TEXT NOT NULL
+)
+
 
 CREATE TABLE comentario (
 	id 				INTEGER PRIMARY KEY NOT NULL,
 	id_evento 		INTEGER NOT NULL,
 	texto 			VARCHAR NOT NULL,
 	username 		VARCHAR NOT NULL
-);
+)
